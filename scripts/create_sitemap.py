abf475fcaca2cb2c9c58fcdb2b53fceeff02ea9b
Gael Varoquaux <gael.varoquaux@normalesup.org>
2019-11-13 01:23:13 +0100

    Add a sitemap for Google search

---
+++ scripts/create_sitemap.py
@@ -0,0 +1,17 @@
+"""
+Generate a sitemap for Google indexing
+"""
+
+import glob
+
+sitemap = open('../sitemap.txt', 'w')
+
+for html_file in glob.glob('../*/html/*.html'):
+    sitemap.write(html_file)
+    sitemap.write('\n')
+
+for md_file in glob.glob('../*.md'):
+    sitemap.write(md_file.replace('.md', '.html'))
+    sitemap.write('\n')
+
+sitemap.close()
