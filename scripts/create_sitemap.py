b45a077accfcde2ddb0ec598a865038de1210a47
Gael Varoquaux <gael.varoquaux@normalesup.org>
2019-11-13 02:39:15 +0100

    Fix sitemap

--- scripts/create_sitemap.py
+++ scripts/create_sitemap.py
@@ -7,11 +7,13 @@ import glob
 sitemap = open('../assets/sitemap.txt', 'w')
 
 for html_file in glob.glob('../*/html/*.html'):
-    sitemap.write(html_file)
+    sitemap.write(html_file.replace('..',
+                    'https://parietal-inria.github.io/DiFuMo'))
     sitemap.write('\n')
 
 for md_file in glob.glob('../*.md'):
-    sitemap.write(md_file.replace('.md', '.html'))
+    sitemap.write(md_file.replace('.md', '.html').replace('..',
+                    'https://parietal-inria.github.io/DiFuMo'))
     sitemap.write('\n')
 
 sitemap.close()
