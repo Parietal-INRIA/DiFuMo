ae86b999e9e6fce604f5b1c34210540e5a97c183
KamalakerDadi <dkamalakarreddy@gmail.com>
2019-12-04 18:32:16 +0100

    256 update

--- scripts/create_each_dimension_md.py
+++ scripts/create_each_dimension_md.py
@@ -9,7 +9,7 @@ fetch_difumo = fetcher['fetch_difumo']
 
 dic = {64: "https://osf.io/ry5fq/download",
        128: "https://osf.io/5kqx7/download",
-       256: "https://osf.io/xkja5/download",
+       256: "https://osf.io/a7wqv/download",
        512: "https://osf.io/unqfz/download",
        1024: "https://osf.io/wr4j3/download",
        }
